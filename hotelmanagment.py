6853ec1ae2dac0c91a6ad6fd41c369b81dc600e0
ReburnedBeans <100004176+ReburnedBeans@users.noreply.github.com>
2022-11-27 21:21:49 -0500

    deleted comment

--- hotelmanagment.py
+++ hotelmanagment.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy #For using SQLAlchemy
 from forms import HotelForm, RegistrationForm, LoginForm
-#comment
 
 #For seeing if the login user is the admin user.
 adminEmail = 'hotelmanagment213@gmail.com'
